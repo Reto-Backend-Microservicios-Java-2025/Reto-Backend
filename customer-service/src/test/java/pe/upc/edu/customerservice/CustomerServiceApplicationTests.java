6d9d01a4662d9941c58bf4926b06c78274553698
Aranda Vallejos, Oscar Gabriel <u202218167@upc.edu.pe>
2025-08-03 22:02:32 -0500

    feat: create customer service

---
+++ customer-service/src/test/java/pe/upc/edu/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
